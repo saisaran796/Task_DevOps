4a4166f78145d9035d1fd3a285938bbb99a32b22
saisaran796 <87978479+saisaran796@users.noreply.github.com>
2022-01-20 14:01:12 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+# import pandas lib as pd
+import pandas as pd
+ 
+# read by default 1st sheet of an excel file
+dataframe1 = pd.read_excel('Individual parameter coverage.xlsx')
+ 
+print(dataframe1)
