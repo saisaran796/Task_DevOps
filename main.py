fed073aff28898dbed5ee699ae1382adb9bc8809
saisaran796 <87978479+saisaran796@users.noreply.github.com>
2022-01-21 08:30:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@
 import pandas as pd
  
 # read by default 1st sheet of an excel file
-dataframe1 = pd.read_excel('Individual parameter coverage.xlsx')
+dataframe1 = pd.read_excel('Individual_parameter_coverage.xlsx')
  
 print(dataframe1)
